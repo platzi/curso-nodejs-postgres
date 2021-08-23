af6ba86cc6e0f8c8171d76ddc84da94d97046d12
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-23 11:43:49 -0400

    :construction: Routing

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-})
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+});
 
 app.listen(port, () => {
   console.log('Mi port' +  port);
