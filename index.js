78ae047a6c9af8565f85fd445853349aa19a247f
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-23 12:22:18 -0400

    :construction: Get ids

--- index.js
+++ index.js
@@ -11,11 +11,34 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000
+    id,
+    name: 'Product 2',
+    price: 2000
   });
 });
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+})
 
 app.listen(port, () => {
   console.log('Mi port' +  port);
