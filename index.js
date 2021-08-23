f49c43f718ada02b2178bacb9290edf25cbf9898
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-23 11:32:56 -0400

    :construction: My server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+})
+
+app.listen(port, () => {
+  console.log('Mi port' +  port);
+});
