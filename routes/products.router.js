d98fd8325825ca4254e7e825f4d9eed4c32bf785
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-23 15:05:23 -0400

    :construction: Status code

--- routes/products.router.js
+++ routes/products.router.js
@@ -23,16 +23,22 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  });
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000
+    });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   });
