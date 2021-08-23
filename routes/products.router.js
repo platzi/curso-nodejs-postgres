8844f00ccfbf41970e2f783585c17491fb1375f5
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-23 14:54:13 -0400

    :construction: Delete and update

--- routes/products.router.js
+++ routes/products.router.js
@@ -36,6 +36,24 @@ router.post('/', (req, res) => {
     message: 'created',
     data: body
   });
-})
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
 
 module.exports = router;
