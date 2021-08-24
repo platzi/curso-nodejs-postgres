d4313f9d07be6c763f09f647a3dd6f5326d11126
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-24 10:43:56 -0400

    :construction: service products

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const faker = require('faker');
+
+const ProductsService = require('./../services/product.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -23,17 +16,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'not found'
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'Product 2',
-      price: 2000
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
---
+++ services/product.service.js
@@ -0,0 +1,44 @@
+const faker = require('faker');
+
+class ProductsService {
+
+  constructor(){
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
+      });
+    }
+  }
+
+  create() {
+
+  }
+
+  find() {
+    return this.products;
+  }
+
+  findOne(id) {
+    return this.products.find(item => item.id === id);
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+
+}
+
+module.exports = ProductsService;
