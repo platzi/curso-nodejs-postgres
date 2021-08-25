a783dc650d1daf03eb43c471b512a1b28a12b6fb
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-25 11:02:07 -0400

    deploy

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
