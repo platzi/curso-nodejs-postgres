7a12626c240b2de206ca3db7dc2087a1939ab58a
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-26 12:31:09 -0400

    :construction: crud with ORM

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,17 +3,17 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const email = Joi.string().email();
 const password = Joi.string().min(8);
-const role = Joi.string().min(5)
+// const role = Joi.string().min(5)
 
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
-  role: role.required()
+  // role: role.required()
 });
 
 const updateUserSchema = Joi.object({
   email: email,
-  role: role,
+  // role: role,
 });
 
 const getUserSchema = Joi.object({
--- services/user.service.js
+++ services/user.service.js
@@ -6,7 +6,8 @@ class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
@@ -15,17 +16,22 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
