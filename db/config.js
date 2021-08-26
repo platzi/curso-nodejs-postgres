7a2a9185786534fb3d06a3b920eebd37808443f8
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-26 14:24:16 -0400

    :construction: migrations

--- db/config.js
+++ db/config.js
@@ -1,17 +1,16 @@
-const { Sequelize } = require('sequelize');
-
 const { config } = require('./../config/config');
-const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: true,
-});
-
-setupModels(sequelize);
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+}
---
+++ db/migrations/20210826181229-create-user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { UserSchema, USER_TABLE } = require('./../models/user.model');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.drop(USER_TABLE);
+  }
+};
