9beacd1a9a93b66683b56b52f75a8015d319d8a4
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-26 11:20:36 -0400

    :construction: implement orm

---
+++ libs/sequelize.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+const { config } = require('./../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const sequelize = new Sequelize(URI, {
+  dialect: 'postgres',
+  logging: true,
+});
+
+module.exports = sequelize;
--- services/product.service.js
+++ services/product.service.js
@@ -1,15 +1,13 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -36,8 +34,8 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
