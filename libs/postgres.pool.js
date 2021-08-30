1c7e8e31e37bb1d5ab63e0cb44ef85853a102ed5
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-30 12:00:48 -0400

    fix error in connection

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -2,16 +2,20 @@ const { Pool } = require('pg');
 
 const { config } = require('./../config/config');
 
-let URI = '';
+const options = {};
 
 if (config.isProd) {
-  URI = config.dbUrl;
+  options.connectionString = config.dbUrl;
+  options.ssl =  {
+    rejectUnauthorized: false
+  };
 } else {
   const USER = encodeURIComponent(config.dbUser);
   const PASSWORD = encodeURIComponent(config.dbPassword);
-  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = URI;
 }
 
-const pool = new Pool({ connectionString: URI });
+const pool = new Pool(options);
 
 module.exports = pool;
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -9,8 +9,10 @@ const options = {
 }
 
 if (config.isProd) {
-  options.ssl = {
-    rejectUnauthorized: false
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 }
 
