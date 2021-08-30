16cdd6156772a4bf3c5c8627aa62461e644b0b49
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-30 12:14:09 -0400

    este si

--- db/config.js
+++ db/config.js
@@ -8,8 +8,10 @@ module.exports = {
   production: {
     url: config.dbUrl,
     dialect: 'postgres',
-    ssl: {
-      rejectUnauthorized: false
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   }
 }
