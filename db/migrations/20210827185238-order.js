74d6cc5cb48241370ae6d59801ace46c78bd0a4c
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-30 12:56:17 -0400

    fix migration order

--- db/migrations/20210827185238-order.js
+++ db/migrations/20210827185238-order.js
@@ -1,10 +1,36 @@
 'use strict';
 
-const { OrderSchema, ORDER_TABLE } = require('./../models/order.model');
+const { ORDER_TABLE } = require('./../models/order.model');
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
   },
 
   down: async (queryInterface) => {
