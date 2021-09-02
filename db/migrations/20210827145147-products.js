438d3fde7653e2208a0d73bb2bd59edc86bc658d
Nicolas Molina Monroy <hi@nicobytes.com>
2021-09-02 11:55:00 -0400

    :construction: work

--- db/migrations/20210827145147-products.js
+++ db/migrations/20210827145147-products.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   down: async (queryInterface) => {
-    await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
   }
 };
